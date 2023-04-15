3043b0811b1522372e5b19ccb6e2ea69026697cd
Maggie von Ebers <vonebersm@gmail.com>
2023-04-15 15:48:12 -0500

    x and y datasets

--- src/utils/rebuild_data.py
+++ src/utils/rebuild_data.py
@@ -10,27 +10,43 @@ import sys
 
 data_dir = sys.argv[1]
 
-def loader(fname, fn):
-    return None
+def loader(fname):
+    x = []
+    y = []
 
-def dumper(content_list, prefix):
+    with open(fname, 'r') as fr:
+        i = 0
+        for line in fr:
+            if i % 2 == 0:
+                x.append(line)
+            else:
+                if line == '0':
+                    y.append('')
+                else:
+                    y.append('the relation between BR1 and BR2 exists.')
+
+            i += 1
+
+    return x, y
+
+def dumper(x, y, prefix):
     # pmid needed?
 
     fw_content = open(prefix + ".x", "w")
     fw_label = open(prefix + ".y", "w")
     
-    for ele in content_list:
-        print(ele[0], file=fw_content)
-        print(ele[1], file=fw_label)
+    for xx, yy in zip(x,y):
+        print(xx, file=fw_content)
+        print(yy, file=fw_label)
 
     fw_content.close()
     fw_label.close()
 
-def worker(fname, prefix, fn):
-    ret = loader(fname, fn)
-    dumper(ret, prefix)
+def worker(fname, prefix):
+    x, y = loader(fname)
+    dumper(x, y, prefix)
 
 
 # right now it expects a json but that will probably be different
 for split in ['train', 'valid', 'test']:
-    worker(os.path.join(f"{data_dir}", f"{split}.json"), os.path.join(f"{data_dir}", f"relis_{split}"), build_target_seq_relis)
+    worker(os.path.join(f"{data_dir}", f"{split}.txt"), os.path.join(f"{data_dir}", f"relis_{split}"))
